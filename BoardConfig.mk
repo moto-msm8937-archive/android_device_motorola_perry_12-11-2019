b466277ddcc2834dca18f39217cb4ca9ad42f279
flagersgit <flagersmail@gmail.com>
2019-08-13 05:43:18 -0400

    perry: comment out bootloader version requirement for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ DEVICE_PATH := device/motorola/perry
 TARGET_OTA_ASSERT_DEVICE := perry,perry_retail
 
 # Board
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+#TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
