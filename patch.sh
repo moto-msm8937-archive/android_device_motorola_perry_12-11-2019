2523b85c872d467b697b6c886a8d2a47e3d9dd12
RebelLion420 <gaigecarlos@gmail.com>
2019-09-25 03:14:24 -0400

    perry: edit patch script
    Change-Id: I220ef3797ac9ade7cc6a0de603a4874633c5bb03
    Signed-off-by: RebelLion420 <gaigecarlos@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-croot
-cd build/core
-#git remote add -f android_build_core https://github.com/GrowtopiaJaw/android_build_core
-#git cherry-pick 928ef1c7d6ba092c1279fe6aa2d5ab2a3683cde8
+cd ../../../build/core
 wget -O whitelist.patch "https://github.com/GrowtopiaJaw/android_build_core/commit/928ef1c7d6ba092c1279fe6aa2d5ab2a3683cde8.patch"
 git am < whitelist.patch
 rm whitelist.patch
+cd -
